62e39517221d9821fe4bdf9c753ec6dd1c8f801b
Tizian Erlenberg <ygh@mail.de>
2022-01-01 18:09:05 +0100

    Caching functionality implemented

--- code/alfaviewCachedLogin.py
+++ code/alfaviewCachedLogin.py
@@ -1,27 +1,50 @@
 import hfuLogin
 import alfaviewEngine
-import json, os
+import json, os, time
 
-def isCache(path='./cache.json'):
+def isCacheFile(path='./cache.json'):
 	return os.path.isfile(path)
 
-def getCache(path='./cache.json'):
-	if isCache(path):
+def getCacheFile(path='./cache.json'):
+	if isCacheFile(path):
 		cacheFile= open(path, 'r')
 		cacheContent= cacheFile.read()
 		cacheFile.close()
 		cacheValues = json.loads(cacheContent)
 
-def setCache(cacheValues, path='./cache.json'):
+		return cacheValues
+	else:
+		return []
+
+def setCacheFile(cacheValues, path='./cache.json'):
 	configContent = json.dumps(configValues)
 
 	configFile= open(path, 'w')
 	configFile.write(configContent)
 	configFile.close()
 
+def cleanCache(cacheValues):
+    newCacheValues=[]
+    cutTime= time.time()-(12*60*60)
+    
+    for item in cacheValues:
+        if item['timestamp'] >= cutTime:
+            newCacheValues.append(item)
+            
+    return newCacheValues
+
+def retrieveCache(cacheValues, key):
+    for item in cacheValues:
+        if item['key'] == key:
+            return item
+
 def start(myRoom, myUsername, myPassword, myName, timeout=15, alfaviewPath, bandWidthUsage='normal', noUpdate=0, skipQuickSetup=1, configDir='.'):
-    hfuLogin.getUrl(myRoom, myUsername, myPassword, myName, geckoLogPath=configDir+'/geckodriver.log', webdriverPath=configDir, timeout=15)
-    alfaviewEngine.start(url, bandWidthUsage='normal', noUpdate=noUpdate, skipQuickSetup=skipQuickSetup, alfaviewPath=alfaviewPath)
-
-
-
+    cacheValues= getCacheFile(configDir + 'cache.json')
+    cacheValues= cleanCache(cacheValues)
+    cachedItem= retrieveCache(cacheValues, myRoom + myUsername + myName)
+    if cachedItem == None:
+        url= hfuLogin.getUrl(myRoom, myUsername, myPassword, myName, geckoLogPath=configDir+'/geckodriver.log', webdriverPath=configDir, timeout=timeout)
+        cachedItem= {'timestamp': time.time(), 'key': myRoom + myUsername + myName, 'url': url}
+        cacheValues.append(cachedItem)
+        
+    alfaviewEngine.start(cachedItem['url'], bandWidthUsage=bandWidthUsage, noUpdate=noUpdate, skipQuickSetup=skipQuickSetup, alfaviewPath=alfaviewPath)
